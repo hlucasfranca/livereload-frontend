db94cfd69e007e61c1e2935cc214687285fd6808
Henrique Lucas França <hlucasfranca@gmail.com>
2017-08-19 19:59:40 -0300

    Componente que consome dados via REST.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/first';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Subscription} from 'rxjs/Subscription';
+
+@Injectable()
+export class StatusService {
+
+  private statusUrl = 'http://localhost:8080/status/';
+
+  constructor(private http: Http) {
+
+  }
+
+  public getStatus(): Observable<any> {
+    return this.http.get(this.statusUrl)
+      .map(res => res.json());
+  }
+
+}
+
+class Maquina {
+  posicao: number;
+  status: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +33,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private timerSubscription: any;
+  private commentsSubscription: Subscription;
+  status: Maquina[];
+
+  constructor(private statusService: StatusService) {
+    this.refreshData();
+  }
+
+  private refreshData(): void {
+    this.commentsSubscription = this.statusService.getStatus().subscribe( (status: Maquina[]) => {
+      this.status = status;
+      this.subscribeToData();
+    });
+  }
+
+  private subscribeToData(): void {
+    this.timerSubscription = Observable.timer(1000).first().subscribe(() => this.refreshData());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent, StatusService} from './app.component';
+import {Http, HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpModule
   ],
-  providers: [],
+  providers: [StatusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
